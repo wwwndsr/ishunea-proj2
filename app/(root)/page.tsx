33171f0c35813a88b7009bb345aa9cd05b13394f
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-09-10 21:20:14 +0300

    страницы регистрации и входа+аутентификация

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const SetupPage = () => {
+  return (
+   <div className="p-4">
+    This is a protected route!
+   </div>
+  )
+}
+
+export default SetupPage;
