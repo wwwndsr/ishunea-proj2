dbd99c7f71b145b4a2115c963f07560a8eef9f0e
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-09-16 01:06:01 +0300

    добавлен navbar с переключением магазинов и авторизацией пользователя

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,28 @@
 import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: {
+          userId
+        },      
+    });
+
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>
-                  This will be a store switcher
-                </div>
+                <StoreSwitcher  items={stores} />
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                    <UserButton afterSignOutUrl="/"/>
